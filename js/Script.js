ac736038ae76a2229ac46f41157dc9643894da92
João Rafael Silva <joaorafa131@gmail.com>
2018-10-16 12:55:15 +0100

    Added Modal for tutorial

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,17 @@
-for(n = 0, n <= 20, n++){
-document.write
-}
+//For the modal
+var modal = document.getElementById("modal");
+var showTutorial = document.getElementById("Tutorial");
+var closebutton = document.getElementById("close");
+
+        showTutorial.onclick = function(){
+            modal.style.display = "block";
+        } 
+        closebutton.onclick = function(){
+            modal.style.display = "none";
+        }
+        window.onclick = function(event){
+            if(event.target == modal){
+                modal.style.display = "none";
+            }
+
+        }
