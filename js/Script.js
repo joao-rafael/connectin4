facc9854d27e0a19a854be0626b1d594c1f3c771
João Rafael Silva <joaorafa131@gmail.com>
2018-10-18 15:05:39 +0100

    Tried to add texture and started board implement

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,27 @@
+//board generation
+function buildBoard(col, row){
+    for(countCol = 0, countCol < col, countCol++){
+        document.getElementById("board").innerHTML += "<div class='col'>";
+        for(countRow= 0, count < row, countRow++){
+            document.getElementById("board").innerHTML += "<div class='slot'></div>";
+        }
+        document.getElementById("board").innerHTML += "</div>";
+    }
+}
+let boardsize = 1;
+
+switch(boardsize){
+    case 1:
+        buildBoard(7, 6);
+        break;
+    case 2:
+        buildBoard(7, 6);
+        break;
+    case 3:
+        buildBoard(7, 6);
+        break;
+}
+
 //For the modal
 var modal = document.getElementById("modal");
 var showTutorial = document.getElementById("Tutorial");
