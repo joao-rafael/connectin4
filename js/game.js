640bf6d39d2e82d81d54a183d5cd083d5e3bb315
João Rafael Silva <joaorafa131@gmail.com>
2018-11-30 18:33:02 +0000

    need help.. :(

--- js/game.js
+++ js/game.js
@@ -9,12 +9,14 @@
   m -> number of rows from connect4. Number of elements from each array.
 **/
 console.log("Game JavaScript File loaded!");
-var n = 7;
-var m = 6;
+var player1;
+var player2;
+var turn;
 function generateGameMatrix(n,m){
   console.log("Initializing game...");
   //Matrix
   let matrix = [];
+  let columnArray = [];
   //visual elements
   const base = document.getElementById("base");
   const board = document.createElement("div");
@@ -22,22 +24,33 @@ function generateGameMatrix(n,m){
   board.className = "board";
   base.appendChild(board);
   console.log("Board base created Successfully");
-  for(let count = 0; count < n; count++) {
+  for(let colCount = 0; colCount < m; colCount++){
+    const col = document.createElement("div");
+    col.setAttribute("id", colCount);
+    col.setAttribute("onclick", "'Fill(id)'");
+    col.className = "col";
+    board.appendChild(col);
+    /*if(colCount = n - 1){
+      colCount = n;
+    }*/
+    console.log("column " + colCount + " created");
+    singleColArray = [];
+    columnArray.push(singleColArray);
+  }
+  for(let count = 0; count < m; count++) {
          //logical matrix
          matrix[count] = [];
-         console.log("Logical matrix line "+ count + " created");
-         //visual elements creation - cols
-         const col = document.createElement("div");
-         col.setAttribute("id", count);
-         col.setAttribute("onclick", "'Fill(id)'");
-         col.className = "col";
-         board.appendChild(col);
+         console.log("Logical matrix row "+ count + " created");
+         //visual elements creation - col
          //fill the cols with cells
-         for(let count2 = 0; count2 < m; count2++){
+         for(let count2 = 0; count2 < n; count2++){
            //put null on the logical matrix
+           console.log("created " + count2 + " element from " + count + " line");
            matrix[count][count2] = null;
+           //console.log("null attribuition");
            //visual elements creation - cells
-           const cellParent = document.getElementById(count);
+           const cellParent = document.getElementById(count2);
+           console.log("attatched to col" + count2);
            const cel = document.createElement("div");
            let colN = String(count);
            let celN = String(count2);
@@ -45,18 +58,23 @@ function generateGameMatrix(n,m){
            //id format colrow like c=0,r=2 ... 02
            cel.setAttribute("id", parseID);
            cel.className = "cell";
+           columnArray[count][count2] = cel.id;
            cellParent.appendChild(cel);
          }
+
   }
+  console.log(columnArray);
+  console.log(matrix);
   console.log("Board Filled Successfully!!!!");
   console.log("Game Matrix Successfully created!");
-  return matrix;
+  return [matrix, columnArray];
 }
 
 function defineCustomSize(){
-  const value = document.getElementById("size");
-  const size = value.value;
-  switch (value) {
+  console.log("user want to define a custom size");
+  const sizevalue = document.getElementById("size");
+  const size = sizevalue.value;
+  switch (sizevalue) {
     case 1:
       console.log("pattern value");
       n = 7;
@@ -76,14 +94,18 @@ function defineCustomSize(){
       n = 7;
       m = 6;
   }
+  return [n,m];
 }
 function Fill(id){
   //TODO
   let selected = document.getElementById()
 }
-function startGame(n,m){
+function startGame(){
   pvpContent.style.display = "none";
-  let gameMatrix = generateGameMatrix(n, m);
+  var sizeArray = defineCustomSize();
+  var n = sizeArray[0];
+  var m = sizeArray[1];
+  generateGameMatrix(n, m);
 }
 const base = document.getElementById("base");
 const preStart = document.getElementById("preStart");
@@ -95,7 +117,6 @@ const pvpContent = document.getElementById("SelectVS");
 const vsPlayer = document.getElementById("vsPlayer");
 
 applyBttn.onclick = function showPVP(){
-  defineCustomSize();
   configContent.style.display = "none";
   pvpContent.style.display = "block";
 }
