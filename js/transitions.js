c76854648588c399ff8537e8a2df614639e05e70
Higor Anjos <higor.araujo.anjos@gmail.com>
2019-01-02 22:30:44 -0300

    Yes you can! [FRONT]

--- js/transitions.js
+++ js/transitions.js
@@ -1,16 +1,16 @@
 /**
-* JavaScript File
-* this is the transitions file.
-* Has the pruporse to switch between layout and
-* to make the connection with the server
-* Made By: João Rafael Silva
-**/
+ * JavaScript File
+ * this is the transitions file.
+ * Has the pruporse to switch between layout and
+ * to make the connection with the server
+ * Made By: João Rafael Silva
+ **/
 console.log("Transitions JavaScript File Loaded!");
 let method = "POST";
-var user = { nick: "", pass: ""}
+var user = { nick: "", pass: "" };
 
 //Functions to switch between contents
-function transition(value){
+function transition(value) {
   switch (value) {
     case 1:
       //TODO
@@ -25,46 +25,43 @@ function transition(value){
       window.location.reload();
       break;
     case 4:
-
       break;
     case 5:
       console.log("Opening register form");
 
     default:
-
   }
 
   return value;
 }
 //Opens the Tutorial Modal:
-function openModal(){
+function openModal() {
   const modal = document.getElementById("tutorialModal");
   const closebutton = document.getElementById("closebttn");
   modal.style.display = "block";
-  window.onclick = function(event){
-    if(event.target == modal){
+  window.onclick = function(event) {
+    if (event.target == modal) {
       modal.style.display = "none";
     }
-  }
-  closebutton.onclick = function(){
+  };
+  closebutton.onclick = function() {
     modal.style.display = "none";
-  }
+  };
 }
 //Generate the intro Form:
 const startForm = document.getElementById("LogInModal");
 const logIn = document.getElementById("LogInBttn");
-logIn.onclick = function enter(){
-    let regUser = document.getElementById('Userbox').value;
-    let regPass = document.getElementById("UserPass").value;
-    register(regUser, regPass);
-    startForm.style.display = "none";
+logIn.onclick = function enter() {
+  let regUser = document.getElementById("Userbox").value;
+  let regPass = document.getElementById("UserPass").value;
+  register(regUser, regPass);
 };
 //Changes to the register form
 const registerForm = document.getElementById("registerForm");
 const signUP = document.getElementById("SignUP");
-signUP.onclick = function launchform(){
+signUP.onclick = function launchform() {
   registerForm.style.display = "block";
-}
+};
 /**
 FETCH FUNCTIONS
 **/
@@ -103,47 +100,37 @@ FETCH FUNCTIONS
 //           alert("error: " + err);
 //         });
 //       }
-function register(nick, pass){
+function register(nick, pass) {
   console.log("chegou");
-  fetch(URL + "register", {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'text/plain'
-    },
-    body: JSON.stringify({"user": nick, "pass" : pass})
-  }).then(function(response){
-            console.log(response);
-            //then(function anonimous(response)
-            //if okay
-              if(response.ok){
-                //response.text().then(console.log);
-                response.json();
-                alert("user: " + nick + " registered/loged with success!");
-                return response.json();
-              }else{//if not okay
-                console.log("Request rejected with the error: " + response.statusText);
-                alert("Failed to register or logIn");
-              }
-    })
+
+  fetch("http://localhost:2044/register", {
+    method: "POST",
+    body: JSON.stringify({ user: nick, pass: pass }),
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(res => res.json())
+    .then(res => console.log(res))
     .catch(err => {
-        alert("error: " + err);
+      alert("error: " + err);
     });
 }
 // ranking
-function ranking(){
-  let lin = parseInt(document.getElementById('ranking-settings-lin').value);
-  let col = parseInt(document.getElementById('ranking-settings-col').value);
-  let data = {'size': {'rows': lin, 'columns': col}};
-  console.log('ranking');
-  fetch(URL+'ranking',{
-    method: 'POST',
+function ranking() {
+  let lin = parseInt(document.getElementById("ranking-settings-lin").value);
+  let col = parseInt(document.getElementById("ranking-settings-col").value);
+  let data = { size: { rows: lin, columns: col } };
+  console.log("ranking");
+  fetch(URL + "ranking", {
+    method: "POST",
     body: JSON.stringify(data)
-    }).then(response=>{
-        return response.json();
-    }).then(j=>{
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(j => {
       loadRanking(j.ranking);
-    }).catch(error=>{
+    })
+    .catch(error => {
       loadTextMessagePopUp(error);
     });
 }
@@ -151,55 +138,55 @@ function ranking(){
 //REGISTER USER
 const regBttn = document.getElementById("registerUser");
 const backBttn = document.getElementById("backBttn");
-regBttn.onclick = function registerUser(){
-    console.log("Register function launched");
-    let regUser = document.getElementById('registerBox').value;
-    let regPass = document.getElementById("registerPassBox").value;
-    let confirmPass = document.getElementById("confirmPassBox").value;
-    if(regUser == "" || regPass == ""){
-      alert("Fill the form correctly!");
-      document.getElementById("registerBox").focus;
-      document.getElementById("registerPassBox").focus;
-      document.getElementById("confirmPassBox").focus;
-      return;
-    }else if(regPass == confirmPass){
-      //register user function
-      registerForm.style.display = "none";
-      register(regUser, regPass);
-    }else if(regPass != confirmPass){
-      alert("Fill the form correctly!");
-      return;
-    }
-  }
-  backBttn.onclick = function backtologin(){
+regBttn.onclick = function registerUser() {
+  console.log("Register function launched");
+  let regUser = document.getElementById("registerBox").value;
+  let regPass = document.getElementById("registerPassBox").value;
+  let confirmPass = document.getElementById("confirmPassBox").value;
+  if (regUser == "" || regPass == "") {
+    alert("Fill the form correctly!");
+    document.getElementById("registerBox").focus;
+    document.getElementById("registerPassBox").focus;
+    document.getElementById("confirmPassBox").focus;
+    return;
+  } else if (regPass == confirmPass) {
+    //register user function
     registerForm.style.display = "none";
-}
+    register(regUser, regPass);
+  } else if (regPass != confirmPass) {
+    alert("Fill the form correctly!");
+    return;
+  }
+};
+backBttn.onclick = function backtologin() {
+  registerForm.style.display = "none";
+};
 //show ranking
-function loadRanking(rank){
-	let  t = document.getElementById('ranking-table');
-	t.innerHTML = "<tr><th>NICK</th><th>VICTORIES.</th><th>GAMES</th></tr>";
-	setTimeout(()=>{
-		for(let i in rank){
-				let r = rank[i];
-				let c, new_r;
-				new_r = document.createElement('tr');
-				c = new_r.insertCell(0);
-				c.innerHTML = r.nick;
-				c = new_r.insertCell(1);
-				c.innerHTML = r.victories;
-				c = new_r.insertCell(2);
-				c.innerHTML = r.games;
-				t.appendChild(new_r, t.children[1]);
-		}
-	}, 0);
+function loadRanking(rank) {
+  let t = document.getElementById("ranking-table");
+  t.innerHTML = "<tr><th>NICK</th><th>VICTORIES.</th><th>GAMES</th></tr>";
+  setTimeout(() => {
+    for (let i in rank) {
+      let r = rank[i];
+      let c, new_r;
+      new_r = document.createElement("tr");
+      c = new_r.insertCell(0);
+      c.innerHTML = r.nick;
+      c = new_r.insertCell(1);
+      c.innerHTML = r.victories;
+      c = new_r.insertCell(2);
+      c.innerHTML = r.games;
+      t.appendChild(new_r, t.children[1]);
+    }
+  }, 0);
 }
 
 //Settings
 const settingsBttn = document.getElementById("settingsBttn");
 const card1 = document.getElementById("preStart");
 const card2 = document.getElementById("settingsContent");
-settingsBttn.onclick = function showSettings(){
+settingsBttn.onclick = function showSettings() {
   card1.style.display = "none";
   card2.style.display = "block";
   //applyBttn.onclick =
-}
+};
